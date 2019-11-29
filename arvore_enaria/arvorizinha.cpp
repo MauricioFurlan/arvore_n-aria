317cf519acf42f4660119af6503c851074b617a3
mauricio.furlan <mauricio.furlan@BRDN54-WS133W>
2019-11-29 17:03:35 -0300

    oi

--- arvore_enaria/arvorizinha.cpp
+++ arvore_enaria/arvorizinha.cpp
@@ -1,45 +1,152 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-#include<stdlib.h>
-#include <corecrt_malloc.h>
-#define true 1
-#define false 0
+#include <stdlib.h>
+#include<string.h>
 
+typedef struct No {
+	char* palavra;
+	int index;
+	No* next_brother;
+	No* next_child;
+}No;
 
-typedef char DATA_NEW[20];
+No* NewNo(char* palavra, int index) {
 
-typedef struct no {
-  DATA_NEW data;
-  struct no* next_child;
-  struct no* next_brother;
-} no;
-
-
-no* NewNo(DATA_NEW data) {
-  no* new_no = (no*)malloc(sizeof(no));
-  new_no->next_child = NULL;
-  new_no->next_brother = NULL;
-  new_no->data = data;
-  return (new_no);
+	int size = strlen(palavra);
+	No* New = (No*)malloc(sizeof(No));
+	New->next_brother = NULL;
+	New->next_child = NULL;
+	New->palavra = (char*)malloc((size + 1) * sizeof(char));
+	strcpy(New->palavra, palavra);
+	New->index = index;
+	return New;
 }
 
-no* Initialize(DATA_NEW data) {
-  return (NewNo(data));
+
+No* initialize(char* palavra, int index) {
+	return NewNo(palavra, index);
 }
 
-no* AddItemTree(no* lista, char data) {
-	no* tmp = (no*)malloc(sizeof(no));
-	tmp = lista;
-	no* child = NewNo(data);
-	return tmp;
+No* BuscaItem(int chavePai, No* root) {
+	if (root == NULL) return (NULL);
+	if (root->index == chavePai) return root;
+	No* p = root->next_child;
+	while (p) {
+		No* resp = BuscaItem(chavePai, p);
+		if (resp) return resp;
+		p = p->next_brother;
+
+	}
+	return (NULL);
+}
+
+bool AddItemTree(char* palavra, int index, int chavePai, No* root) {
+	No* pai = BuscaItem(chavePai, root);
+	if (!pai) return false;
+	No* filho = NewNo(palavra, index);
+
+	No* p = pai->next_child;
+	if (!p) pai->next_child = filho;
+	else {
+		while (p->next_brother) {
+			p = p->next_brother;
+		}
+		p->next_brother = filho;
+	}
+	return true;
+}
+
+//void AddItemTreiner(char* palavra, int index, int chavePai, No* root){
+//	if (root == NULL) {
+//		return;
+//	}
+//	if (root->next_child == NULL){
+//		No* 
+//	}
+//
+//
+//}
+
+void imprimir(No* root) {
+	if (root == NULL) return;
+	printf("%d(", root->index);
+	printf("%s", root->palavra);
+
+	No* tmp = root->next_child;
+	while (tmp) {
+		imprimir(tmp);
+		tmp = tmp->next_brother;
+	}
+	printf(")");
+}
+
+//No* Delete(int num, No* root) {
+//	if (root == NULL) return (NULL);
+//	if (root->palavra == num) return root;
+//	No* p = root->next_child;
+//	while (p) {
+//		No* resp = BuscaItem(num, p);
+//		if (resp) return resp;
+//		p = p->next_brother;
+//	}
+//	return (NULL);
+//}
+
+int menu() {
+	int op;
+	printf("Pokemon\n");
+	printf("[1] - Add Treinador: \n");
+	printf("[2] - Add Elemento: \n");
+	printf("Escolha: ");
+	scanf_s("%d", &op);
+	return op;
 }
 
 int main() {
-  no* lista = Initialize('oi');
 
-  printf("%s", lista->data);
 
-	
 
-  return 0;
+
+	char inicio[] = "Inicio";
+	int x = 10;
+	No* lista = initialize(inicio, 0);
+	do {
+		int x = menu();
+
+
+		switch (x) {
+
+		case 1:
+			int index;
+			int count;
+			printf("Quantos treinadores?");
+			scanf_s("%d", &count);
+			for (int i = 0; i < count; i++) {
+				char nome[50];
+				printf("Digite o Index do treinador\n");
+				printf("Index: ");
+				scanf_s("%d", &index);
+				printf("Digite o Nome do treinador\n");
+				printf("Nome: ");
+				scanf("%s", nome);
+				AddItemTree(nome, index, 0, lista);
+			}
+
+
+		case 2:
+			system("CLS");
+			imprimir(lista);
+			printf("\nDigite o Index do treinador\n");
+			printf("Index: ");
+			scanf_s("%d", &index);
+		}
+
+		imprimir(lista);
+		//printf("%s", arvore->palavra);
+		//No* id = BuscaItem(40, arvore);
+		//imprimir(arvore);
+	} while (x != 0);
+
+
+	return 0;
 }
