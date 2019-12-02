2e4d490b6f704051546e4c2f764abe8d5af4e5e7
mauricio.furlan <mauricio.furlan@BRDN54-WS133W>
2019-12-02 16:58:54 -0300

    arvore

--- arvore_enaria/arvorizinha.cpp
+++ arvore_enaria/arvorizinha.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
 typedef struct No {
 	char* palavra;
@@ -44,7 +44,6 @@ bool AddItemTree(char* palavra, int index, int chavePai, No* root) {
 	No* pai = BuscaItem(chavePai, root);
 	if (!pai) return false;
 	No* filho = NewNo(palavra, index);
-
 	No* p = pai->next_child;
 	if (!p) pai->next_child = filho;
 	else {
@@ -56,21 +55,48 @@ bool AddItemTree(char* palavra, int index, int chavePai, No* root) {
 	return true;
 }
 
-//void AddItemTreiner(char* palavra, int index, int chavePai, No* root){
-//	if (root == NULL) {
-//		return;
-//	}
-//	if (root->next_child == NULL){
-//		No* 
-//	}
-//
-//
-//}
+
+
+void AddItemElement(char* palavra, int index, int chavePai, No* root){
+	if (root == NULL) {
+		return;
+	}
+	No* pai = BuscaItem(chavePai, root);
+	No* element = NewNo(palavra, index);
+	No* p = pai;
+	if (p->next_child != NULL) {
+			p = p->next_child;
+			while (p->next_brother != NULL) {
+				p = p->next_brother;
+			}
+			p->next_brother = element;
+	}else{
+	pai->next_child = element;
+	}
+}
+void AddItemPokemon(char* palavra, int index, int chavePai, No* root) {
+	if (root == NULL) {
+		return;
+	}
+	No* pai = BuscaItem(chavePai, root);
+	No* pokemon = NewNo(palavra, index);
+	No* p = pai;
+	if (p->next_child != NULL) {
+	
+		while (p->next_brother != NULL) {
+			p = p->next_brother;
+		}
+		p->next_brother = pokemon;
+	}
+	else {
+		pai->next_child = pokemon;
+	}
+}
 
 void imprimir(No* root) {
 	if (root == NULL) return;
-	printf("%d(", root->index);
-	printf("%s", root->palavra);
+	printf("%d( ", root->index);
+	printf("%s ", root->palavra);
 
 	No* tmp = root->next_child;
 	while (tmp) {
@@ -93,10 +119,11 @@ void imprimir(No* root) {
 //}
 
 int menu() {
+
 	int op;
 	printf("Pokemon\n");
 	printf("[1] - Add Treinador: \n");
-	printf("[2] - Add Elemento: \n");
+	printf("[2] - Add Buscar: \n");
 	printf("Escolha: ");
 	scanf_s("%d", &op);
 	return op;
@@ -104,12 +131,10 @@ int menu() {
 
 int main() {
 
-
-
-
 	char inicio[] = "Inicio";
 	int x = 10;
 	No* lista = initialize(inicio, 0);
+	int index = 1;
 	do {
 		int x = menu();
 
@@ -117,36 +142,42 @@ int main() {
 		switch (x) {
 
 		case 1:
-			int index;
+			
 			int count;
+			int index_element;
+			int indexpoke;
 			printf("Quantos treinadores?");
 			scanf_s("%d", &count);
 			for (int i = 0; i < count; i++) {
 				char nome[50];
-				printf("Digite o Index do treinador\n");
-				printf("Index: ");
-				scanf_s("%d", &index);
-				printf("Digite o Nome do treinador\n");
+				char element[30];
+				char pokemon[10];
+				printf("Digite o nome do treinador:\n");
 				printf("Nome: ");
 				scanf("%s", nome);
 				AddItemTree(nome, index, 0, lista);
+				imprimir(lista);
+				printf("\nQual treinador deseja add o elemento? [index treinador]:");
+				scanf("%d", &index_element);
+				printf("Elemento:");
+				scanf("%s", element);
+				AddItemElement(element, index, index_element, lista);
+				printf("Qual elemento voce deseja adicionar o pokemon? [index element]:");
+				scanf("%d", &indexpoke);
+				printf("Pokemon: ");
+				scanf("%s", pokemon);
+				AddItemPokemon(pokemon, index, 1, lista);
+				++index;
 			}
-
 
 		case 2:
 			system("CLS");
 			imprimir(lista);
-			printf("\nDigite o Index do treinador\n");
-			printf("Index: ");
-			scanf_s("%d", &index);
+	
 		}
 
-		imprimir(lista);
-		//printf("%s", arvore->palavra);
-		//No* id = BuscaItem(40, arvore);
-		//imprimir(arvore);
 	} while (x != 0);
-
+	printf("Fim");
 
 	return 0;
 }
